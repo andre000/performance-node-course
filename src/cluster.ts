e123f49a1b0802e69ec5c62e75b15bf267c930cf
André Adriano <andre@dev-next.com>
2020-06-23 11:24:02 -0300

    :sparkles: Added multiple clusters

--- src/cluster.ts
+++ src/cluster.ts
@@ -6,6 +6,9 @@ const isMainThread = cluster.isMaster;
 if (isMainThread) {
   console.log(chalk`{bold.gray Forking thread...}`);
   cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
 } else {
   console.log(chalk`{bold Running child thread}`);
   import('./server');
