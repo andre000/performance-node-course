f9b4a3a4169548113f92b93606aadab78951cd27
André Adriano <andre@dev-next.com>
2020-06-23 11:45:55 -0300

    :alembic: Changed parameters for benchmark test

--- src/cluster.ts
+++ src/cluster.ts
@@ -1,13 +1,12 @@
 import cluster from 'cluster';
 import chalk from 'chalk';
 
+process.env.UV_THREADPOOL_SIZE = '1';
+
 const isMainThread = cluster.isMaster;
 
 if (isMainThread) {
   console.log(chalk`{bold.gray Forking thread...}`);
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
   cluster.fork();
 } else {
   console.log(chalk`{bold Running child thread}`);
