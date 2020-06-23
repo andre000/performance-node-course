549c8e0aabee219157ccc0b30c93e1d17b20a709
André Adriano <andre@dev-next.com>
2020-06-23 11:02:04 -0300

    :sparkles: Added cluster function

---
+++ src/cluster.ts
@@ -0,0 +1,12 @@
+import cluster from 'cluster';
+import chalk from 'chalk';
+
+const isMainThread = cluster.isMaster;
+
+if (isMainThread) {
+  console.log(chalk`{bold.gray Forking thread...}`);
+  cluster.fork();
+} else {
+  console.log(chalk`{bold Running child thread}`);
+  import('./server');
+}
